f07c26c161cd296b19dd7240919c1acaded87116
xiaolin <xiaolinyo@gmail.com>
2026-09-12 16:58:57 +0800

    fix: import dashboard card enum

--- lib/views/dashboard/widgets/codex_accounts.dart
+++ lib/views/dashboard/widgets/codex_accounts.dart
@@ -1,4 +1,5 @@
 import 'package:fl_clash/common/common.dart';
+import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/features/codex/codex_account_snapshot.dart';
 import 'package:fl_clash/l10n/l10n.dart';
 import 'package:fl_clash/widgets/widgets.dart';
